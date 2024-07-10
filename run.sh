815677b6f8c472751931ae39e5269b90ce9ec193
John Lee <admin@decorultra.com>
2024-07-10 08:33:11 +0000

    chore: run script

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ python3 manage.py read customer
 python3 manage.py read customer-update
 python3 manage.py read product
 python3 manage.py read setpart
+python3 manage.py read order
 
 python3 manage.py sync customer
 python3 manage.py sync product
