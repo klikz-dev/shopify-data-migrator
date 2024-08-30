586ab83825ca8fc838455febb48e250052ef7b07
Murrell Johnson <dev@klikz.us>
2024-08-30 21:18:41 +0000

    chore: script

--- scripts/run.sh
+++ scripts/run.sh
@@ -13,10 +13,12 @@ python3 ./manage.py read customer customer-update
 python3 ./manage.py read order
 python3 ./manage.py read company
 
-# python3 ./manage.py sync product
-# python3 ./manage.py sync product
-# python3 ./manage.py sync variant
-# python3 ./manage.py sync customer
-# python3 ./manage.py sync customer
+python3 ./manage.py sync product
+python3 ./manage.py sync product
+python3 ./manage.py sync variant
+python3 ./manage.py sync customer
+python3 ./manage.py sync customer
+# python3 ./manage.py sync company
+# python3 ./manage.py sync company
 # python3 ./manage.py sync order
 # python3 ./manage.py sync order
