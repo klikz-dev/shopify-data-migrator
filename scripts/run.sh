59074700c6d6a7c8f5ae4d89df8ce9e5d4397371
Murrell Johnson <dev@klikz.us>
2024-10-16 15:37:41 +0000

    chore: automation script

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,6 +9,10 @@ python3 ./manage.py delete order
 python3 ./manage.py delete customer
 python3 ./manage.py delete product
 python3 ./manage.py delete company
+python3 ./manage.py delete order
+python3 ./manage.py delete customer
+python3 ./manage.py delete product
+python3 ./manage.py delete company
 
 # Read
 python3 ./manage.py read product setpart
@@ -19,5 +23,15 @@ python3 ./manage.py read company
 # Sync
 python3 ./manage.py sync product
 python3 ./manage.py sync customer
+python3 ./manage.py sync product
+python3 ./manage.py sync customer
+python3 ./manage.py sync product
+python3 ./manage.py sync customer
+
 python3 ./manage.py sync company
+python3 ./manage.py sync company
+python3 ./manage.py sync company
+
+python3 ./manage.py sync order
+python3 ./manage.py sync order
 python3 ./manage.py sync order
